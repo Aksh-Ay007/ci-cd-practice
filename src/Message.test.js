ed18bf4de4e8c5666dfb3434e08a3c4933f99473
Aksh-Ay007 <akshayjyothip@gmail.com>
2025-08-06 02:33:41 +0400

    final

--- src/Message.test.js
+++ src/Message.test.js
@@ -3,8 +3,7 @@ import { render, screen } from '@testing-library/react';
 import Message from './Message';
 
 test('renders message text correctly', () => {
-  render(<Message text="Some other message" />);
-  const element = screen.getByText(/Test CI\/CD message/i); // looks for old text
+  render(<Message text="Test CI/CD message" />);
+  const element = screen.getByText(/Test CI\/CD message/i);
   expect(element).toBeInTheDocument();
 });
-
