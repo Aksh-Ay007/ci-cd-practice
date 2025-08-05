3ff694a4f34931cae0964c4f50e409f6ed655c99
Aksh-Ay007 <akshayjyothip@gmail.com>
2025-08-06 02:28:39 +0400

    test error

--- src/Message.test.js
+++ src/Message.test.js
@@ -3,7 +3,8 @@ import { render, screen } from '@testing-library/react';
 import Message from './Message';
 
 test('renders message text correctly', () => {
-  render(<Message text="Test CI/CD message" />);
-  const element = screen.getByText(/Test CI\/CD message/i);
+  render(<Message text="Some other message" />);
+  const element = screen.getByText(/Test CI\/CD message/i); // looks for old text
   expect(element).toBeInTheDocument();
 });
+
