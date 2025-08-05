393518914c7585211d2e65ebc0b20ccfc07cee09
Aksh-Ay007 <akshayjyothip@gmail.com>
2025-08-06 01:44:45 +0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Message from './Message';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>React CI/CD Demo</h1>
+      <Message text="Welcome to the CI/CD pipeline demo app!" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Message.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Message({ text }) {
+  return <p style={{ color: 'teal', fontSize: '20px' }}>{text}</p>;
+}
+
+export default Message;
